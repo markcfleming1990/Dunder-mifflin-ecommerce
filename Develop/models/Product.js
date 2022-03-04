b03d696abb614eaebc14da9918335261e03525ab
Mark <markcfleming1990@gmail.com>
2022-03-04 13:58:26 -0500

    worked on column structure

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,6 +10,40 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      product_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+        validate: {
+          isDecimal: true
+        }
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: "10",
+        validate: {
+          isNumeric: true
+        }
+      },
+
+        references: {
+          model: 'post',
+          key: 'id'7-=90-2= require('')
+          validate: 
+        }
+      },
+    
   },
   {
     sequelize,
